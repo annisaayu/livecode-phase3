c78fa526be521409e93c0c70c12beece46e58cc3
annisaayu <annisaayu569@gmail.com>
2018-07-20 11:34:06 +0700

    tic-tac-toe

--- LiveCode/src/screens/PlayScreen.js
+++ LiveCode/src/screens/PlayScreen.js
@@ -22,8 +22,42 @@ class PlayScreen extends Component {
     }
   }
 
-  touchBoard(row, column) {
+  computerTurn = ()  => {
+    const boardData = this.props.redux.board
+    let getBoard = false
+    let nullBoard = []
+    for (var i = 0; i <  boardData.length; i++) {
+      for (var j = 0; j < boardData[i].length; j++)  {
+        if (boardData[i][j] == 0) {
+           boardData[i][j] = 2
+           getBoard = true
+          break
+        }
+      }
+      if (getBoard) {
+        break; 
+      }
+    }
+  
+    this.props.touchBoard(boardData, 0)
+  }
 
+  touchBoard(row, column) {
+    let boardData = this.props.redux.board
+    let player = this.props.redux.activePlayer
+
+    if (boardData[x][y] == 0) {
+      if (activePlayer == 0) {
+          boardData[x][y] = 1
+          activePlayer = 1
+        } else {
+          boardData[x][y] = 2
+          activePlayer = 0
+        }
+        const newBoard = boardData
+        this.props.touchBoard(newBoard, 1)
+        this.computerTurn()
+    }
   }
 
   render() {
@@ -34,37 +68,48 @@ class PlayScreen extends Component {
           {}
           <View style={styles.rowBoard}>
             <TouchableOpacity style={styles.xo} onPress={() => {this.touchBoard(0,0)}}>
-              <Text>{board[0][0]}</Text>
+            <View style={{width: 50, height: 50, backgroundColor: 'skyblue',}}>
+                <Text>{board[0][0]}</Text>
+              </View>
             </TouchableOpacity>
             <TouchableOpacity style={styles.xo} onPress={() => {this.touchBoard(0,1)}}>
+            <View style={{width: 50, height: 50, backgroundColor: 'skyblue',}}>
               <Text>{board[0][1]}</Text>
+            </View>
             </TouchableOpacity>
             <TouchableOpacity style={styles.xo} onPress={() => {this.touchBoard(0,2)}}>
+            <View style={{width: 50, height: 50, backgroundColor: 'skyblue',}}>
               <Text>{board[0][2]}</Text>
+            </View>
             </TouchableOpacity>
           </View>
 
           <View style={styles.rowBoard}>
             <TouchableOpacity  style={styles.xo} onPress={() => {this.touchBoard(1,0)}}>
+            <View style={{width: 50, height: 50, backgroundColor: 'skyblue',}}>
               <Text>{board[1][0]}</Text>
+            </View>
             </TouchableOpacity>
             <TouchableOpacity  style={styles.xo} onPress={() => {this.touchBoard(1,1)}}>
+            <View style={{width: 50, height: 50, backgroundColor: 'skyblue',}}>
               <Text>{board[1][1]}</Text>
+            </View>
             </TouchableOpacity>
             <TouchableOpacity  style={styles.xo} onPress={() => {this.touchBoard(1,2)}}>
-              <Text>{board[1][2]}</Text>
+            <View style={{width: 50, height: 50, backgroundColor: 'skyblue',}}><Text>{board[1][2]}</Text></View>
             </TouchableOpacity>
           </View>
 
           <View style={styles.rowBoard}>
             <TouchableOpacity  style={styles.xo} onPress={() => {this.touchBoard(2,0)}}>
-              <Text>{board[2][0]}</Text>
+              <View style={{width: 50, height: 50, backgroundColor: 'skyblue',}}><Text>{board[2][0]}</Text></View>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.xo} onPress={() => {this.touchBoard(2,0)}}>
-              <Text>{board[2][1]}</Text>
+            <TouchableOpacity style={styles.xo} onPress={() => {this.touchBoard(2,1)}}>
+              <View style={{width: 50, height: 50, backgroundColor: 'skyblue',}}><Text>{board[2][1]}</Text></View>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.xo} onPress={() => {this.touchBoard(2,0)}}>
-              <Text>{board[2][2]}</Text>
+            <TouchableOpacity style={styles.xo} onPress={() => {this.touchBoard(2,2)}}>
+              <View style={{width: 50, height: 50, backgroundColor: 'skyblue',}}>
+              <Text>{board[2][2]}</Text></View>
             </TouchableOpacity>
           </View>
         </View>
